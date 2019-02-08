ab4f6ab01403b8d1709190f1221a0cae4109565a
Bulik László <laszlo.bulik@ksh.hu>
2019-02-08 18:07:13 +0100

    Fix: user specified pattern does not work.

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 const interpolateString = require("interpolate-string");
 
-const interpolate = (target, context) => {
+const interpolate = (target, context, pattern) => {
     if (typeof target === "string") {
-        return interpolateString(target, context);
+        return interpolateString(target, context, pattern);
     } else if (Array.isArray(target)) {
         const a = [];
         for (let value of target) {
-            a.push(interpolate(value, context));
+            a.push(interpolate(value, context, pattern));
         }
         return a;
     } else if (target && typeof target === "object") {
         const o = {};
         for (let p in target) {
             if (target.hasOwnProperty(p)) {
-                o[p] = interpolate(target[p], context);
+                o[p] = interpolate(target[p], context, pattern);
             }
         }
         return o;
